022e96fc9862259067c91b9c9a0b3a2f69ae7fa4
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-04-26 16:26:36 +0300

    Update chack.py

--- chack.py
+++ chack.py
@@ -1,18 +1,32 @@
 import requests
 import os
 from time import sleep
-from colorama import Fore, Style, Back
+from colorama import Fore, Style, Back, init
 from termcolor import colored
 os.system("clear")
 
-print(f'''{Fore.YELLOW}
+init()
+
+R = f"\033[91;1m"  # Red
+G = f"\033[92;1m"  # Green
+B = f"\033[94;1m"  # Blue
+Y = f"\033[93;1m"  # Yellow
+C = f"\033[96;1m"  # Cyan
+M = f"\033[95;1m"  # Magenta
+W = f"\033[97;1m"  # White
+
+sign = f"{G}[{B}*{G}]{W}"
+ERROR = f"{Y}[{R}!{Y}]{R}"
+INFO = f'{B}[{G}INFO{B}]'
+
+print(fr'''{Y}
    ________              __  _       __                _ __     
   / ____/ /_  ___  _____/ /_| |     / /__  ____  _____(_) /____ 
- / /   / __ \/ _ \/ ___/ //_/ | /| / / _ \/ __ \/ ___/ / __/ _ \\
+ / /   / __ \/ _ \/ ___/ //_/ | /| / / _ \/ __ \/ ___/ / __/ _ \
 / /___/ / / /  __/ /__/ ,<  | |/ |/ /  __/ /_/ (__  ) / /_/  __/
 \____/_/ /_/\___/\___/_/|_| |__/|__/\___/ .___/____/_/\__/\___/ 
                                        /_/                      
-{Style.RESET_ALL}''')
+{W}''')
 
 print(f"{Back.RED} It checks the status of the website whether it is available or not {Style.RESET_ALL}")
 print(1*"\n")
@@ -22,24 +36,24 @@ def check_status(url):
         status_code = response.status_code
 
         if status_code == 200:
-            print(f"{Fore.WHITE}[{Fore.GREEN}INFO{Fore.WHITE}] {Fore.BLUE}Good website condition: {status_code} - OK{Style.RESET_ALL}")
+            print(f"{INFO} Good website condition: {status_code} - OK{Style.RESET_ALL}")
         elif status_code == 404:
-            print(f"{Fore.RED}Error! Status Code: {status_code} - Not Found{Style.RESET_ALL}")
+            print(f"{R}Error! Status Code: {status_code} - Not Found{Style.RESET_ALL}")
         elif status_code == 403:
-            print(f"{Fore.YELLOW}Error! Status Code: {status_code} - Forbidden{Style.RESET_ALL}")
+            print(f"{Y}Error! Status Code: {status_code} - Forbidden{Style.RESET_ALL}")
         elif status_code == 401:
-            print(f"{Fore.BLUE}Error! Status Code: {status_code} - Unauthorized{Style.RESET_ALL}")
+            print(f"{B}Error! Status Code: {status_code} - Unauthorized{Style.RESET_ALL}")
         elif status_code == 500:
-            print(f"{Fore.YELLOW}Error! Status Code: {status_code} - Internal Server Error{Style.RESET_ALL}")
+            print(f"{Y}Error! Status Code: {status_code} - Internal Server Error{Style.RESET_ALL}")
         elif status_code == 301:
-            print(f"{Fore.WHITE}Redirect! Status Code: {status_code} - Moved Permanently{Style.RESET_ALL}")
+            print(f"{M}Redirect! Status Code: {status_code} - Moved Permanently{Style.RESET_ALL}")
         elif status_code == 307:
-            print(f"{Fore.WHITE}Redirect! Status Code: {status_code} - Temporary Redirect{Style.RESET_ALL}")
+            print(f"{C}Redirect! Status Code: {status_code} - Temporary Redirect{Style.RESET_ALL}")
         else:
-            print(f"{Fore.YELLOW}[{Fore.RED}!{Fore.YELLOW}] Unknown Status Code:{Style.RESET_ALL} {status_code}")
+            print(f"{Y}[{R}!{Y}] Unknown Status Code:{W} {status_code}")
 
     except requests.exceptions.RequestException as e:
-        print(f"{Fore.YELLOW}[{Fore.RED}Error{Fore.YELLOW}]: {e}{Style.RESET_ALL}")
+        print(f"{ERROR}: {e}{W}")
 
-url_to_check = input(f"{Fore.GREEN}[+] {Fore.BLUE}please Enter URL wepsite: {Fore.YELLOW}")
+url_to_check = input(f"{sign} please Enter URL wepsite: {Y}")
 check_status(url_to_check)
